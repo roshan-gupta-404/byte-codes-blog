5e8ab93696b25d50da4c67aa91f8fe918b8a19eb
Roshan <webdev404.error@gmail.com>
2024-01-27 18:00:52 +0530

    created user authentication system.

---
+++ src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthServices {
+    client = new Client();
+    account;
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client);
+    }
+
+    // SIGNING UP THE USER
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                //login the user
+                this.login({ email, password })
+            }
+            else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+    // LOGIN
+    async login({ email, password }) {
+        try {
+            return await account.createEmailSession(email, password);
+        } catch (error) {
+            throw error
+        }
+    }
+
+    // GETTING CURRENT USER/ACCOUNT
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error", error);
+        }
+        return null
+    }
+
+    async logout(){
+        try {
+            await this.account.deleteSessions(); // this will delete all sessions.
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error", error);
+        }
+    }
+}
+
+const authServices = new AuthServices()
+
+export default authServices
