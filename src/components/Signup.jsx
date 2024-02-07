b11521a81e679cdad2228bd68ef7d44e27e6996b
Roshan <webdev404.error@gmail.com>
2024-02-07 18:15:38 +0530

    auth key signup restriction added.

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -5,31 +5,38 @@ import Button from './form_components/Button'
 import { useForm } from 'react-hook-form'
 import authServices from '../appwrite/auth'
 import { useDispatch } from 'react-redux'
-import {login} from '../store/authSlice'
+import { login } from '../store/authSlice'
 import { useNavigate } from 'react-router-dom'
+import conf from '../conf/conf'
 
 function Signup() {
-    const {register, handleSubmit, formState} = useForm()
-    const [submissionError , setSubmissionError] = useState("")
+    const { register, handleSubmit, formState } = useForm()
+    const [submissionError, setSubmissionError] = useState("")
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const {errors} = formState
+    const { errors } = formState
 
-    const submit = async(data)=>{
-        setSubmissionError("")
-       const user = await authServices.createAccount(data)
-       if(user){
-        const userData = await authServices.getCurrentUser()
-        if(userData){
-            dispatch(login({userData}))
-            navigate('/')
+    const submit = async (data) => {
+        if (data.auth_key === conf.authKey) {
+            setSubmissionError("")
+            const user = await authServices.createAccount(data)
+            if (user) {
+                const userData = await authServices.getCurrentUser()
+                if (userData) {
+                    dispatch(login({ userData }))
+                    navigate('/')
+                }
+            }
+            else {
+                setSubmissionError("Some error occured. Try again later.")
+            }
         }
-       }
-       else{
-        setSubmissionError("Some error occured. Try again later.")
-       }
+        else{
+            setSubmissionError('Auth Key Invalid')
+        }
+
     }
-    
+
     return (
         <div className='my-6'>
             <Container>
@@ -45,66 +52,66 @@ function Signup() {
                     </div>
 
                     <form className='w-4/5 mx-auto' onSubmit={handleSubmit(submit)}>
-                    <div className = 'mb-2'>
-                        <Input
-                            label='Name:-'
-                            placeholder='Enter your name'
-                            {...register('name',{
+                        <div className='mb-2'>
+                            <Input
+                                label='Name:-'
+                                placeholder='Enter your name'
+                                {...register('name', {
                                     required: {
                                         value: true,
                                         message: "Field is required",
                                     },
                                 })}
-                        />
-                        {<span className='text-red-500'> &nbsp; {errors.name?.message}</span>}
+                            />
+                            {<span className='text-red-500'> &nbsp; {errors.name?.message}</span>}
                         </div>
-                        <div className = 'mb-2'>
-                        <Input
-                            label='Email:-'
-                            type={'email'}
-                            placeholder='Enter your Email'
-                            {...register('email',{
+                        <div className='mb-2'>
+                            <Input
+                                label='Email:-'
+                                type={'email'}
+                                placeholder='Enter your Email'
+                                {...register('email', {
                                     required: {
                                         value: true,
                                         message: "Field is required",
                                     },
                                 })}
-                        />
-                        {<span className='text-red-500'> &nbsp; {errors.email?.message}</span>}
+                            />
+                            {<span className='text-red-500'> &nbsp; {errors.email?.message}</span>}
                         </div>
-                        <div className = 'mb-2'>
-                        <Input
-                            label='Password:-'
-                            type={'password'}
-                            placeholder='Enter your Password'
-                            {...register('password',{
+                        <div className='mb-2'>
+                            <Input
+                                label='Password:-'
+                                type={'password'}
+                                placeholder='Enter your Password'
+                                {...register('password', {
                                     required: {
                                         value: true,
                                         message: "Field is required",
                                     },
                                 })}
-                        />
-                        {<span className='text-red-500'> &nbsp; {errors.password?.message}</span>}
+                            />
+                            {<span className='text-red-500'> &nbsp; {errors.password?.message}</span>}
                         </div>
-                        <div className = 'mb-2'>
-                        <Input
-                            label='Auth Key:-'
-                            placeholder='Enter your Auth Key'
-                            {...register('auth_key',{
+                        <div className='mb-2'>
+                            <Input
+                                label='Auth Key:-'
+                                placeholder='Enter your Auth Key'
+                                {...register('auth_key', {
                                     required: {
                                         value: true,
                                         message: "Field is required",
                                     },
                                 })}
-                        />
-                        {<span className='text-red-500'> &nbsp; {errors.auth_key?.message}</span>}
+                            />
+                            {<span className='text-red-500'> &nbsp; {errors.auth_key?.message}</span>}
                         </div>
                         <Button
                             className={`text-white text-2xl block font-semibold my-4 bg-blue-700 w-1/2 mx-auto p-1 rounded-md hover:bg-blue-800`}
                         >
                             {'Submit'}
                         </Button>
-                    </form>                    
+                    </form>
                 </div>
                 {submissionError && <div className='text-red-600 text-xl w-fit mx-auto my-6'>{submissionError}</div>}
             </Container>
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,8 @@ const conf = {
     appwriteDbId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    rteApiKey:String(import.meta.env.VITE_RTE_API_KEY)
+    rteApiKey:String(import.meta.env.VITE_RTE_API_KEY),
+    authKey:String(import.meta.env.VITE_BCB_AUTH_KEY)
 }
 
 export default conf
