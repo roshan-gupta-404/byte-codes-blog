e902a9040df784e7c44f8330b9877824b257ec8b
Roshan <webdev404.error@gmail.com>
2024-02-12 19:02:01 +0530

    Content structure inmproved

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {Container} from '../components/Index'
+import { Container } from '../components/Index'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import services from '../appwrite/config'
 import parse from 'html-react-parser';
@@ -8,9 +8,9 @@ import Button from '../components/form_components/Button';
 import '../postPage.css'
 
 function Post() {
-    const {slug} = useParams()
+    const { slug } = useParams()
     const [post, setPost] = useState()
-    const[loader, setLoader] = useState(true)
+    const [loader, setLoader] = useState(true)
     const navigate = useNavigate()
 
     const userData = useSelector((state) => state.user);
@@ -19,81 +19,81 @@ function Post() {
     function convertTextToDate(dateText) {
         // Parse the date text into a JavaScript Date object
         const parsedDate = new Date(dateText);
-      
+
         // Check if the parsed date is valid
         if (isNaN(parsedDate.getTime())) {
-          // If the parsed date is invalid, return null or throw an error
-          return null;
+            // If the parsed date is invalid, return null or throw an error
+            return null;
         }
-      
-        return parsedDate;
-      }
 
-    function deletePost(){
-       services.deletePost(post.slug).then((status)=>{
-        if(status){
-            services.deleteFile(post.featured_image).then(()=>{
-                navigate('/')
-            })
-        }
-       })
-       .catch((err)=>{
-        throw err
-       })
-        
+        return parsedDate;
     }
-    useEffect(()=>{
+
+    function deletePost() {
+        services.deletePost(post.slug).then((status) => {
+            if (status) {
+                services.deleteFile(post.featured_image).then(() => {
+                    navigate('/')
+                })
+            }
+        })
+            .catch((err) => {
+                throw err
+            })
+
+    }
+    useEffect(() => {
         services.getPost(slug).then((post) => {
-            if(post){
+            if (post) {
                 setPost(post)
                 setLoader(false)
             }
         }).catch((err) => {
             throw err
         });
-    },[])
+    }, [])
 
-    return ( loader ? (<div className='text-white my-4'>
-    <Container>
-        <div id='blog-card-container'
-        className='w-3/4 flex justify-center mx-auto'
-        >
-           Loading...
-        </div>
-    </Container>
-</div>) : (<>
-            <div className='text-white'>
-                <Container>
-                    <div className=''>
-                        <div className='relative bg-slate-800 h-56 flex flex-col items-center pt-14'>
+    return (loader ? (<div className='text-white my-4'>
+        <Container>
+            <div id='blog-card-container'
+                className='w-3/4 flex justify-center mx-auto'
+            >
+                Loading...
+            </div>
+        </Container>
+    </div>) : (<>
+        <div className='text-white'>
+            <Container>
+                <div className=''>
+                    <div className='relative bg-slate-800 h-56 flex flex-col items-center pt-14'>
                         {isAuthor && (
-                        <div className="absolute right-20 top-20">
-                            <Link to={`/edit-post/${post.$id}`}>
-                                <Button className={`bg-green-700 w-16 mb-2 rounded-xl`}>
-                                    Edit
-                                </Button>
-                            </Link>
+                            <div className="absolute right-20 top-20">
+                                <Link to={`/edit-post/${post.$id}`}>
+                                    <Button className={`bg-green-700 w-16 mb-2 rounded-xl`}>
+                                        Edit
+                                    </Button>
+                                </Link>
 
-                            <Button className={`bg-red-700 w-16 mt-2 rounded-xl`} onClick={deletePost}>
-                                Delete
-                            </Button>
-                        </div>
-                    )}
-                            <h1 className='text-3xl font-semibold font-mono mb-4'>{post.title}</h1>
-                            <p className='font-thin text-sm'>{convertTextToDate(post.date).toLocaleDateString()}</p>
-                        </div>
-                        <div className='bg-slate-900 flex flex-col items-center pt-40'>
-                            <div className=' max-w-80 absolute -translate-y-60 rounded-xl overflow-hidden'>
-                                <img src={services.getFilePreview(post.featured_image)} />
+                                <Button className={`bg-red-700 w-16 mt-2 rounded-xl`} onClick={deletePost}>
+                                    Delete
+                                </Button>
                             </div>
-                            <div className='w-3/4 mt- mb-8 '>
-                               {parse(post.content)}
-                            </div>
+                        )}
+                        <h1 className='text-3xl font-semibold font-mono mb-4 text-center'>{post.title}</h1>
+                        <p className='font-thin text-sm'>{convertTextToDate(post.date).toLocaleDateString()}</p>
+                    </div>
+                    <div className='bg-slate-900 flex flex-col items-center pt-40'>
+                        <div className=' max-w-80 absolute -translate-y-60 rounded-xl overflow-hidden'>
+                            <img src={services.getFilePreview(post.featured_image)} />
+                        </div>
+                        <div className='w-3/4 mt- mb-8 '>
+                            {parse(post.content)}
                         </div>
                     </div>
-                </Container>
-            </div>
-        </>)
+                </div>
+            </Container>
+        </div>
+    </>)
     )
 }
 
